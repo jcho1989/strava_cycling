b3cf54ffc587b83a84063a168d6472314ab9826c
Samuel Leiendecker <leiendeckers@worldaware.com>
2024-07-16 22:29:05 -0400

    styles

--- src/components/BasePage/BasePage.jsx
+++ src/components/BasePage/BasePage.jsx
@@ -1,4 +1,4 @@
-import {AppShell, Button} from '@mantine/core';
+import {AppShell, Badge, Button, Card, Flex, Group, Image, Text} from '@mantine/core';
 
 import AuthorizedApp from '../AuthorizedApp/AuthorizedApp';
 
@@ -15,21 +15,46 @@ export default function BasePage() {
 
   return (
     <AppShell
-      header={{ height: 60 }}
       padding="md"
     >
       <AppShell.Main>
-        <div>
+        <Flex
+        style={{ height: '100vh' }} // Ensure the flex container takes the full height of the viewport
+        direction="column"
+        align="center"
+        justify="center"
+      >
+
           {isLoading ? (
             <p>Authenticating... Please wait</p>
           ) : isLoggedIn ? (
             <AuthorizedApp/>
           ) : (
-            <Button onClick={handleLogin}>
+            <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image
+                src="https://i0.wp.com/www.nfrcycling.com/wp-content/uploads/2020/10/strava-club.jpg"
+                height={160}
+                alt="Norway"
+              />
+            </Card.Section>
+      
+            <Group justify="space-between" mt="md" mb="xs">
+              <Text fw={500}>Strava Segments</Text>
+              <Badge color="pink">HELL YEAH</Badge>
+            </Group>
+      
+            <Text size="sm" c="dimmed">
+              <span>Strava test app to do something with segments I think.</span>
+            </Text>
+      
+            <Button color="blue" fullWidth mt="md" radius="md" onClick={handleLogin}>
               Authenticate with Strava
             </Button>
-        )}
-      </div>
+          </Card>
+          )}
+
+      </Flex>
       </AppShell.Main>
     </AppShell>
   );
